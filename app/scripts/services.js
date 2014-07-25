ef265a526f5cb90e6f3b5c0aaf088fb560c63e6c
johnvehr <johnvehr@yahoo.com>
2014-07-25 11:17:17 -0600

    Add empty service methods for responders

--- app/scripts/services.js
+++ app/scripts/services.js
@@ -6,6 +6,16 @@ angular.module('WMISoapBuilder.services', ['angular-websql'])
  */
 
 .factory('Responders', function() {
+  var responders = [];
+
+  return {
+    createNewResponder: function(responderData){
+      //todo => pass data to websql methods
+    },
+    all: function() {
+      //todo => return all responders 
+    }
+  }
 
 })
 
