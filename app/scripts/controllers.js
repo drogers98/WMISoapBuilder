8fb298762dbd902fbc2c9758b53ed26dfba30fa6
johnvehr <johnvehr@yahoo.com>
2014-07-25 11:13:29 -0600

    Build first resp. controller only method

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -12,6 +12,23 @@ angular.module('WMISoapBuilder.controllers', ['angular-websql'])
   //$scope.responder = Responders.all();
   $scope.trainingLevels = ['WFA','WAFA','WFR', 'WEMT'];
 
+  $scope.newResponder = function(responder) {
+    var attributes = {
+      responder: {
+        firstName: responder.firstName,
+        lastName: responder.lastName,
+        trainingLevel: responder.trainingLevel
+      }
+    };
+    //below call service methods - todo => implement service methods
+    var responderData = angular.toJson(attributes);
+    Responders.createNewResponder(responderData);
+    $scope.responders.push(responderData);
+    //after save link to soaps
+    $state.responderSoapsPage();
+  }
+
+
 })
 
 .controller('SoapCtrl', function($scope, Soaps) {
