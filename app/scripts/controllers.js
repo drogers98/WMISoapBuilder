fc312cdf323676f4bed85ddd88a55b0b54f7d633
johnvehr <johnvehr@yahoo.com>
2014-07-28 09:19:33 -0600

    add few scope properties for soaps

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -31,5 +31,9 @@ angular.module('WMISoapBuilder.controllers', ['angular-websql'])
 
 })
 
-.controller('SoapCtrl', function($scope, Soaps) {
+.controller('SoapCtrl', function($scope,$state,Soaps) {
+  $scope.genders = ['Male', 'Female', 'Other';
+  $scope.severities = [0,1,2,3,4,5,6,7,8,9,10];
+  $scope.onsets = ['Sudden', 'Gradual'];
+
 })
