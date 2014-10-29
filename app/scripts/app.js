81bcf06333366f89b27e06c6b8ff3fba90d479af
Daniel Rogers <rogers@eyebytesolutions.com>
2014-10-29 14:00:00 -0600

    dr merge

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -17,6 +17,7 @@ angular.module('WMISoapBuilder', ['ionic', 'WMISoapBuilder.controllers', 'WMISoa
     // for form inputs)
     if(window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
     }
     if(window.StatusBar) {
       // org.apache.cordova.statusbar required
